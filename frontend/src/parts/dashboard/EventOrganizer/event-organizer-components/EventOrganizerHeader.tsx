df8db682fc3cd733f1b589d61d638099037eb55a
Arnav Agrawal <99753899+arnavzzz@users.noreply.github.com>
2026-09-20 22:25:14 +0530

    Completing Organizer dashboard with HTML 6: Creating Header 4

--- frontend/src/parts/dashboard/EventOrganizer/event-organizer-components/EventOrganizerHeader.tsx
+++ frontend/src/parts/dashboard/EventOrganizer/event-organizer-components/EventOrganizerHeader.tsx
@@ -1,6 +1,7 @@
 import styles from "../../../../styles/EventOrganizer.module.css"
 import RightSideHeader from "./header-components/RightSideHeader.tsx";
 import LeftSideHeader from "./header-components/LeftSideHeader.tsx";
+import MiddleSideHeader from "./header-components/MiddleSideHeader.tsx";
 
 interface EventOrganizerHeaderProps {
     isDark: boolean;
@@ -12,7 +13,7 @@ function EventOrganizerHeader({isDark, onToggle, onLogOut}:EventOrganizerHeaderP
     return(
         <header className={isDark?styles.eventHeader:styles.eventHeaderDark}>
             <LeftSideHeader isDark={isDark} />
-
+            <MiddleSideHeader isDark={isDark} />
             <RightSideHeader isDark={isDark} onToggle={onToggle} onLogOut={onLogOut} />
         </header>
     )
--- frontend/src/parts/dashboard/EventOrganizer/event-organizer-components/header-components/MiddleSideHeader.tsx
+++ frontend/src/parts/dashboard/EventOrganizer/event-organizer-components/header-components/MiddleSideHeader.tsx
@@ -1,1 +1,17 @@
 import styles from "../../../../../styles/EventOrganizer.module.css"
+
+interface MiddleSideHeaderProps{
+    isDark: boolean;
+}
+
+function MiddleSideHeader ({isDark}:MiddleSideHeaderProps) {
+    return(
+        <div className={isDark? styles.middleHeader : styles.middleHeaderDark}>
+            <div>
+                <input type="search"/>
+            </div>
+        </div>
+    )
+}
+
+export default MiddleSideHeader
