1e63d75feb3f60c26db31d31f91ec2b95fc1f41b
Arnav Agrawal <99753899+arnavzzz@users.noreply.github.com>
2026-09-21 16:41:54 +0530

    Completing Organizer dashboard with HTML 11: Working with Main Body 4

--- frontend/src/parts/dashboard/EventOrganizer/EventOrganizerMainBody.tsx
+++ frontend/src/parts/dashboard/EventOrganizer/EventOrganizerMainBody.tsx
@@ -3,6 +3,12 @@ import Overview from "./pages/Overview.tsx";
 import Events from "./pages/Events.tsx";
 import Setting from "./pages/Setting.tsx"
 import AiPlanner from "./pages/AiPlanner.tsx";
+import Venue from "./pages/Venue.tsx";
+import Vendor from "./pages/Vendor.tsx";
+import Budget from "./pages/Budget.tsx";
+import Schedule from "./pages/Schedule.tsx";
+import Staff from "./pages/Staff.tsx";
+import Guest from "./pages/Guest.tsx";
 
 interface EventOrganizerMainBodyProps {
     isDark: boolean;
@@ -21,6 +27,24 @@ function EventOrganizerMainBody({isDark, activeTab}: EventOrganizerMainBodyProps
             )}
             {activeTab === "ai-planner"&& (
                 <AiPlanner isDark={isDark} />
+            )}
+            {activeTab === "venues" && (
+                <Venue isDark={isDark} />
+            )}
+            {activeTab === "vendors" && (
+                <Vendor isDark={isDark} />
+            )}
+            {activeTab === "budget" && (
+                <Budget isDark={isDark} />
+            )}
+            {activeTab === "schedule" && (
+                <Schedule isDark={isDark} />
+            )}
+            {activeTab === "staff" && (
+                <Staff isDark={isDark} />
+            )}
+            {activeTab == "guest" && (
+                <Guest isDark={isDark}/>
             )}
             {activeTab === "setting" && (
                 <Setting isDark={isDark}/>
